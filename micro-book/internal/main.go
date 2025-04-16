114f3dfa99ab22fe182b3b69ef28003c038d7c72
TCM1205619139 <tangchenming0011@gmail.com>
2025-04-17 00:28:12 +0800

    refactor: 稍微拆分一下结构

--- micro-book/internal/main.go
+++ micro-book/internal/main.go
@@ -13,7 +13,14 @@ import (
 
 func main() {
 	server := gin.Default()
+	db := initDatabase(server)
+	user := initUser(db)
 
+	user.RegisterRoutes(server.Group("/user"))
+	server.Run(":8080")
+}
+
+func initDatabase(service *gin.Engine) *gorm.DB {
 	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:13306)/webook"), &gorm.Config{})
 	if err != nil {
 		panic("数据库初始化错误")
@@ -22,11 +29,14 @@ func main() {
 	if err != nil {
 		panic("表初始化错误")
 	}
+	return db
+}
+
+func initUser(db *gorm.DB) *web.UserHandler {
 	ud := dao.NewUserDAO(db)
 	ur := repository.NewUserRepository(ud)
 	usvc := service.NewUserService(ur)
 	user := web.NewUserHandler(usvc)
-	user.RegisterRoutes(server.Group("/user"))
 
-	server.Run(":8080")
+	return user
 }
